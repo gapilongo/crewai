64fd0cea9d61fe3860984eca7a6a3c9cad947648
gapilongo <nibniw@gmail.com>
2024-04-09 11:55:21 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from crewai import Crew
+from tasks import MeetingPrepTasks
+from agents import MeetingPreAgents
+import os
+
+os.environ['OPENAI_API_KEY']
+os.environ['LANGCHAIN_API_KEY']
+os.environ['LANGCHAIN_TRACING_V2']
+os.environ['LANGCHAIN_ENDPOINT']
+def main():
+    print("## Welcome to the Meeting Prep Crew")
+    print('-------------------------------')
+    meeting_participants = input("What are the emails for the participants (other than you) in the meeting?\n")
+    meeting_context = input("What is the context of the meeting?\n")
+    meeting_objective = input("What is your objective for this meeting?\n")
+
+    tasks = MeetingPrepTasks()
+    agents = MeetingPreAgents()
+
+    research_agent = agents.research_agent()
+    industry_analysis_agent = agents.industry_analysis_agent()
+    meeting_strategy_agent = agents.meeting_strategy_agent()
+    summary_and_briefing_agent = agents.summary_and_briefing_agent()
+
+    research_task = tasks.research_task(research_agent, meeting_participants, meeting_context)
+    industry_analysis_task = tasks.industry_analysis_task(industry_analysis_agent, meeting_participants, meeting_context)
+    meeting_strategy_task = tasks.meeting_strategy_task(meeting_strategy_agent, meeting_context, meeting_objective)
+    summary_and_briefing_task = tasks.summary_and_briefing_task(summary_and_briefing_agent, meeting_context, meeting_objective)
+
+    meeting_strategy_task.context = [research_task, industry_analysis_task]
+    summary_and_briefing_task.context = [research_task, industry_analysis_task, meeting_strategy_task]
+
+    # Create Crew responsible for Copy
+    crew = Crew(
+        agents=[
+            research_agent,
+            industry_analysis_agent,
+            meeting_strategy_agent,
+            summary_and_briefing_agent
+        ],
+        tasks=[
+            research_task,
+            industry_analysis_task,
+            meeting_strategy_task,
+            summary_and_briefing_task
+        ]
+    )
+
+    result = crew.kickoff()
+
+
+    # Print results
+    print("\n\n################################################")
+    print("## Here is the result")
+    print("################################################\n")
+    print(result)
+
+if __name__ == "__main__":
+    main()
